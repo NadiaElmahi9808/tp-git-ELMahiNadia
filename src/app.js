cf7a8d7d4456b822923eb1d34904183e74b12df6
Nadia  EL Mahi <nadiaelmahi47@gmail.com>
2026-03-07 15:13:28 +0000

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,19 +1,9 @@
 console.log("Hello from B - conflit");
 
-<<<<<<< HEAD
-console.log("Update from A");
+
+console.log("Conflict from A");
 console.log("Feauture FF");
-console.log("Feauture FF");
-console.log("Feature FF");
 console.log("Feature NO FF");
-console.log("Feauture FF");
-console.log("Feauture FF");
-console.log("Feauture NO FF");
-console.log("Feauture NO FF");
-console.log("Feauture NO FF");
 console.log("Squash 1");
 console.log("Squash 2");
-console.log("Squash 2");
-=======
-console.log("Update from A");
->>>>>>> 364cb0033c243be87328e2e6e779286da7115798
+
